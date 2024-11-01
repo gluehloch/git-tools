355f635510cbe7b98b0cc6be2d8e30384d244a73
Andre Winkler <gluehloch@googlemail.com>
2024-11-01 09:20:34 +0100

    WIP: Serving some files.

--- git-browser/src/main/java/de/awtools/git/browser/GitRepositoryBrowser.java
+++ git-browser/src/main/java/de/awtools/git/browser/GitRepositoryBrowser.java
@@ -1,13 +1,24 @@
 package de.awtools.git.browser;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.Path;
 import java.time.ZonedDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @CrossOrigin
@@ -19,8 +30,25 @@ public class GitRepositoryBrowser {
     private String repositoryPath;
 
     @GetMapping("/browse")
-    public String browse(@RequestParam("path") String path) {
-        return ZonedDateTime.now().toString();
+    @ResponseBody
+    public ResponseEntity<InputStreamResource> browse(@RequestParam("path") String path) {
+        final var filePath = Path.of(repositoryPath, path);
+        final var file = filePath.toFile();
+        if (file.isDirectory()) {
+
+        } else if (file.exists()) {
+            try {
+                System.out.println("File: " + file.getAbsolutePath());
+                
+                Optional<MediaType> mimeTypeOptional = MediaTypeFactory.getMediaType(file.getName());
+                InputStream is = new FileInputStream(file);
+                return ResponseEntity.ok().contentType(mimeTypeOptional.get()).body(new InputStreamResource(is));
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        }
+        ByteArrayInputStream is = new ByteArrayInputStream(ZonedDateTime.now().toString().getBytes());
+        return  ResponseEntity.ok().contentType(MediaType.TEXT_PLAIN).body(new InputStreamResource(is));
     }
 
 }
