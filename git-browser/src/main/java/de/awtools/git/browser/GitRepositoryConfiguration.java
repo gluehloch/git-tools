b5eb4a3a10781a64908c52c7444a3816adb31ebc
gluehloch <gluehloch@googlemail.com>
2024-11-15 19:53:29 +0100

    under construction...

--- git-browser/src/main/java/de/awtools/git/browser/GitRepositoryConfiguration.java
+++ git-browser/src/main/java/de/awtools/git/browser/GitRepositoryConfiguration.java
@@ -5,9 +5,14 @@ import java.nio.file.Path;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
+@PropertySource(ignoreResourceNotFound = true, value = {
+        "file:${user.home}/.git-tools.properties",
+        "file:${AWTOOLS_CONFDIR}/git-tools/git-tools.properties"
+})
 public class GitRepositoryConfiguration {
 
     @Value("${de.awtools.git.browser.repository.path}")
