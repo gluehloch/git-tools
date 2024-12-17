07889c8963507f8ac35d51fd059ff50cc3cd302a
gluehloch <gluehloch@googlemail.com>
2024-12-17 06:59:11 +0100

    FIX: the fileFinder.

--- git-browser/src/main/java/de/awtools/git/browser/FileFinder.java
+++ git-browser/src/main/java/de/awtools/git/browser/FileFinder.java
@@ -6,16 +6,20 @@ public class FileFinder {
 
     public static File findFile(String name, File file) {
         File[] list = file.listFiles();
+        File found = null;
         if (list != null) {
             for (File fil : list) {
                 if (fil.isDirectory()) {
-                    return findFile(name, fil);
+                    found = findFile(name, fil);
                 } else if (name.equalsIgnoreCase(fil.getName())) {
-                    return fil;
+                    found = fil;
+                }
+                if (found != null) {
+                    break;
                 }
             }
         }
-        return null;
+        return found;
     }
 
 }
